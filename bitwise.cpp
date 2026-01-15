4f5332a455c07d649ee3d12fd14bc11da91508ad
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 17:45:07 +0530

    this is for bitwise

---
+++ bitwise.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 11, y=5,z;
+    z =x&y;
+    cout<<z<<endl;
+    z =x|y;
+    cout<<z<<endl;
+    int m = 6,l;
+
+    l = m<<1;
+    cout<<l;
+
+    return 0;
+}
