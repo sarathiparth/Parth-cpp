598359e790117b37bf0f7735e4772b770b9b22e7
sarathiparth <sarathigiriparth@gmail.com>
2026-01-16 01:12:58 +0530

    this program is for leap year

---
+++ leap_year.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cin>>year;
+    if(year%4 == 0 && year%100 == 0 && year%400 == 0){
+        cout<<"leap year";
+    }
+    else{
+        cout<<"not leap year";
+    }
+
+    return 0;
+}
