6863dbdf57f0d57a89ac997277de96658c66dca7
sarathiparth <sarathigiriparth@gmail.com>
2026-01-16 01:02:17 +0530

    this program is for switch cases

---
+++ switch.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int day;
+    cin>>day;
+
+    switch(day)
+    {
+        case 1:
+        {
+            cout << "sun";
+            break;
+        }
+        case 2:
+        {
+            cout << "mon";
+            break;
+        }
+        case 3:
+        {
+            cout << "tue";
+            break;
+        }
+        case 4:
+        {
+            cout << "wed";
+            break;
+        }
+        case 5:
+        {
+            cout << "thu";
+            break;
+        }
+        case 6:
+        {
+            cout << "fri";
+            break;
+        }
+        case 7:
+        {
+            cout << "sat";
+            break;
+        }
+        default:
+        {
+            cout << "Invalid day";
+            break;
+        }
+    }
+
+    return 0;
+}
