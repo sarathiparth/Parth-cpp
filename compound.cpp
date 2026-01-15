19d71b66a19205f29ca7b21f58bf717a994e3fc1
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 17:17:45 +0530

    this is program for compuding concept

---
+++ compound.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum = 10;
+    int mult = 10;
+    int x = 5;
+
+    sum += x;
+    mult *= x;
+
+
+    cout<< sum<<endl;
+    cout<< mult<<endl;
+
+    return 0;
+}
