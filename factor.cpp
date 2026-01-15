d74dd11df3be559dc0671a9eba130ded314e6570
sarathiparth <sarathigiriparth@gmail.com>
2026-01-16 01:40:30 +0530

    this program is factor

---
+++ factor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        if(n%i == 0){
+            cout<<i<<endl;
+        }
+        i++;
+    }
+    return 0;
+}
