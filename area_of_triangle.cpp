7d0148e01c2c1bbb2698ed213299ca117f96c9d9
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 15:27:16 +0530

    this is program for area of triangle

---
+++ area_of_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int b , h , area;
+    cout<<"insert you breath";
+    cin>>b;
+    cout<<"insert your height";
+    cin>>h;
+
+    area = (b*h)/2;
+
+    cout<< area;
+
+    return 0;
+}
