969ff26e25dce7dfd18c0795008313ccac0121e6
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 15:58:11 +0530

    this is program for volume of cylinder

---
+++ volume_cylinder.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int r , h;
+    float volume;
+    cout<<"enter you r and h";
+    cin>>r>>h;
+    volume = (float)3.14*r*r*h;
+    cout<<volume;
+    return 0;
+}
