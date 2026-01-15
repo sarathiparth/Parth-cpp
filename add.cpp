981a1f9ab1da4dbc63de33ffffb2f7a931e75542
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 13:33:24 +0530

    this is add program

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a , b , c;
+    cout<<"enter number a ";
+    cin>>a;
+    cout<<"enter number b";
+    cin>>b;
+    c = a + b ;
+    cout<<c<<" here is the addition of a + b";
+    return 0;
+
+
+}
