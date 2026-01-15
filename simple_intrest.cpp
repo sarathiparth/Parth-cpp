52bc604fd8ccd5bd38ca009718552f3c31ec5bd3
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 15:54:23 +0530

    this is program si

---
+++ simple_intrest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int p , t , r;
+    int simple_intrest;
+    cout<<"enter your p \n"<<"enter you t \n"<<"enter you r \nṇ";
+
+    cin>>p;
+    cin>>t;
+    cin>>r;
+
+    simple_intrest = (p*r*t)/100;
+
+    cout<<simple_intrest;
+
+    return 0;
+
+
+}
