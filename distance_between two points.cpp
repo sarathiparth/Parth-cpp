2d8d1ec399cbbeb889930c3a03dae4d0c882cce0
sarathiparth <sarathigiriparth@gmail.com>
2026-01-15 16:00:58 +0530

    this is program for distance cal

---
+++ distance_between two points.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x , y ,z,w;
+    int m ,o ;
+    cin>>x>>y>>z>>w;
+
+    m = x-z;
+    o = y-w;
+    cout<<m<<o;
+
+    return 0;
+
+}
