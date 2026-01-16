f0381106929d01579db1022f9156f507ca7834c7
sarathiparth <sarathigiriparth@gmail.com>
2026-01-16 12:08:39 +0530

    this program is for seprate number

---
+++ seprate_digit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    int r;
+    cin>>n;
+
+    while(n>0){
+        r = n%10;
+        cout<<r<<endl;
+        n = n/10;
+    }
+
+    return 0;
+}
