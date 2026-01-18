12b22acd05c8c96deb9417b8877ae26d890f58fa
sarathiparth <sarathigiriparth@gmail.com>
2026-01-18 15:45:56 +0530

    this program is for string function

---
+++ string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char s[10]= "hello";
+    char a[] = {'g','g','s'};
+
+    for(int i = 0 ; i<10 ; i++){
+        cout<<s[i];
+        cout<<a[i];
+    }
+
+    return 0;
+    
+
+}
