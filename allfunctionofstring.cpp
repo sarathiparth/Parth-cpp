fb72707cd8e4cffd5eb46b96d8e08ee2adafad0d
sarathiparth <sarathigiriparth@gmail.com>
2026-01-18 21:02:32 +0530

    this program tells all the command in string header

--- allfunctionofstring.cpp
+++ allfunctionofstring.cpp
@@ -12,6 +12,16 @@ int main(){
     a.resize(50);
     cout<<a.max_size()<<endl;
     a.clear();
+    a.append("hello ji");
+    cout<<a<<endl;
+    a.insert(4,"nahi");
+    cout<<a<<endl;
+    a.replace(4,6,"nn");
+    cout<<a<<endl;
+    a.push_back('i');
+    cout<<a<<endl;
+    a.pop_back();
+    cout<<a<<endl;
 
     return 0;
 }
