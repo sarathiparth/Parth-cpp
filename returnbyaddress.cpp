feddb6ff6e3962ae024662630d30d54cb3d1e925
sarathiparth <sarathigiriparth@gmail.com>
2026-01-19 23:16:03 +0530

    this program is for return by address

---
+++ returnbyaddress.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int* fun(int size) {
+
+    int* p = new int[size];
+
+    for(int i = 0; i < size; i++) {
+        p[i] = i + 1;
+    }
+
+    return p;   
+}
+
+int main() {
+
+    int* ptr = fun(5);
+
+    for(int i = 0; i < 5; i++) {
+        cout << ptr[i] << " ";
+    }
+
+    delete[] ptr;   
+
+    return 0;
+}
