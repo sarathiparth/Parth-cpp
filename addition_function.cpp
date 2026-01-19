86c37a8dccad99ded80c7abdeae7c6c169caa853
sarathiparth <sarathigiriparth@gmail.com>
2026-01-19 10:04:03 +0530

    this program is now can give max also

--- addition_function.cpp
+++ addition_function.cpp
@@ -8,9 +8,23 @@ int add(int x , int y){
     return z;
 }
 
+int Max(int a,int b,int c){
+
+{if(a>b && b>c){
+    return a;
+}
+else if(c>b){
+    cout<<c;
+}
+else{
+    return b;
+}
+}}
+
 int main()
 {
     cout<<add(10,5);
+    cout<<Max(7,10,5);
 
     return 0;
 }
