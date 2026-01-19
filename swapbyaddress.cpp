c96882486c3332e2f6acdd61ecd34f89137d4a58
sarathiparth <sarathigiriparth@gmail.com>
2026-01-19 14:39:54 +0530

    this program is for address

--- swapbyaddress.cpp
+++ swapbyaddress.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 void swap(int *x , int *y){
-    int *temp = x;
-    x = y;
-    y = temp;
+    int temp ;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 int main(){
     int a = 10 , b = 20;
-    swap(a,b);
+    swap(&a,&b);
     cout<<a<<" "<<b;
     return 0;
 }
