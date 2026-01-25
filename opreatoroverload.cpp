6501181ef2e69c81ac9838da0116a8a8cb9ca9fb
sarathiparth <sarathigiriparth@gmail.com>
2026-01-25 16:33:32 +0530

    its a cpp code

---
+++ opreatoroverload.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    int real;
+    int img;
+
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        img = i;
+    }
+
+    Complex add(const Complex& x) {
+        Complex temp;
+        temp.real = real + x.real;
+        temp.img = img + x.img;
+        return temp;
+    }
+
+    void display() {
+        cout << real << " + " << img << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(7, 3);
+    Complex c2(8, 4);
+    Complex c3;
+
+    c3 = c1.add(c2);
+
+    c3.display();
+    return 0;
+}
