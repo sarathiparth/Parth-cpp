d06e8df0b5b0906351355e2feb200820938d5128
sarathiparth <sarathigiriparth@gmail.com>
2026-01-27 02:32:15 +0530

    this cpp program

--- inheritance1.cpp
+++ inheritance1.cpp
@@ -1,53 +1,79 @@
 #include<iostream>
 using namespace std;
 
-class rectangle{
+class rectangle {
 
-    private :
+private:
     int lenght;
     int breath;
 
-    public :
-    rectangle(int l = 0 ,int b = 0){
+public:
+    rectangle(int l = 0, int b = 0) {
         lenght = l;
         breath = b;
     }
 
-    int getlenght(){
-        cout<<lenght<<endl;
+    int getlenght() {
+        return lenght;
     }
 
-    int getbreath(){
-        cout<<breath<<endl;
+    int getbreath() {
+        return breath;
     }
 
-    void setlenght(int l){
+    void setlenght(int l) {
         lenght = l;
     }
 
-    void setbreath(int b){
+    void setbreath(int b) {
         breath = b;
     }
 
-    int area(){
-        return breath*lenght;
+    int area() {
+        return breath * lenght;
     }
 
-    int peri(){
-        return 2*(lenght+breath);
+    int peri() {
+        return 2 * (lenght + breath);
     }
 };
 
-int main(){
+class cuboid : public rectangle {
+
+private:
+    int height;
+
+public:
+    cuboid(int l = 0, int b = 0, int h = 0) : rectangle(l, b) {
+        height = h;
+    }
+
+    int getheight() {
+        return height;
+    }
+
+    void setheight(int h) {
+        height = h;
+    }
+
+    int volume() {
+        return getlenght() * getbreath() * height;
+    }
+};
+
+int main() {
+
     rectangle c;
     c.setbreath(4);
     c.setlenght(5);
 
-    c.getbreath();
-    c.getlenght();
+    cout << c.getbreath() << endl;
+    cout << c.getlenght() << endl;
+    cout << c.area() << endl;
+    cout << c.peri() << endl;
 
-    cout<<c.area()<<endl;
-    cout<<c.peri();
+    cuboid cb(4, 5, 6);
+    cout << cb.volume() << endl;
 
     return 0;
 }
